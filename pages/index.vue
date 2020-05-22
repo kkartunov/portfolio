844b2629e5637e493e76283a1ab8dc1585449f4f
Kiril Kartunov <kiril.kartunov@gmail.com>
2020-05-22 13:09:03 +0300

    WIP message

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,19 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
-        kkartunov-site
+        Portfolio-site
       </h1>
       <h2 class="subtitle">
         Kiril Kartunov portfolio site
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h3>WIP</h3>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import Logo from '~/components/Logo.vue'
-
-export default Vue.extend({
-  components: {
-    Logo
-  }
-})
 </script>
 
 <style>
